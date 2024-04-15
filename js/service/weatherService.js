4d56a17836dfedcb6da2c935362da25837032cba
jRocha <jose.f.rocha92@gmail.com>
2024-04-15 12:40:21 +0100

    Add the weather icon, current temp, and max/min

--- js/service/weatherService.js
+++ js/service/weatherService.js
@@ -1,7 +1,8 @@
 async function getWeatherByCity (city) {
     const params = {
-        APPID: '96d64b7a16971bbc205c578e1eecf518',
-        q: city
+        APPID: 'e6191a31ced8e1da7523fcaacc800cf4',
+        q: city,
+        units: 'metric'
     };
 
     const URL = `https://api.openweathermap.org/data/2.5/weather?${new URLSearchParams(params).toString()}`;
--- js/view/mapView.js
+++ js/view/mapView.js
@@ -4,10 +4,11 @@ function render() {
   const container = document.querySelector('#container');
   container.innerHTML = '';
 
+  //search
   const item = document.createElement('div');
   item.className = `input-group mb-3 mt-3`;
   item.innerHTML = `<input type="text" class="form-control" placeholder="Enter a city name" aria-label="Enter a city name" aria-describedby="button-addon2" id="city-name">
-                    <button class="btn btn-outline-secondary" type="button" id="btn-weather">Search</button>`
+                    <button class="btn btn-outline-secondary" type="button" id="btn-weather">Search</button>`;
 
   container.appendChild(item);
 
@@ -20,10 +21,53 @@ function render() {
 
     const result = await weatherService.getWeatherByCity(cityInput.value);
     console.log(result);
-  }
 
-  console.log(document.querySelector('#btn-weather'));
-};
+    // table header creation
+    if (!document.querySelector('#weather-table')) {
+      const table = document.createElement('table');
+      table.id = 'weather-table';
+      table.className = 'table';
+      container.appendChild(table);
 
+      const tableHeader = document.createElement('thead');
+      tableHeader.innerHTML = `
+        <tr>
+          <th>City Name</th>
+          <th></th>
+          <th>Weather</th>
+          <th>current Temp</th>
+          <th>Max Temp</th>
+          <th>Min Temp</th>
+          <th>Action</th>
+        </tr>
+      `;
+      table.appendChild(tableHeader);
+    }
+
+    // adding row
+    const tableBody = document.createElement('tbody');
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+      <td>${result.name}</td>
+      <td><img src="https://openweathermap.org/img/wn/${result.weather[0].icon}@2x.png" alt="image icon" style="width: 35px"></td>
+      <td>${result.weather[0].main}</td>
+      <td>${result.main.temp} Cº</td>
+      <td>${result.main.temp_max} Cº</td>
+      <td>${result.main.temp_min} Cº</td>
+      <td><button class="btn btn-danger delete-btn">Delete</button></td>
+    `;
+    tableBody.appendChild(newRow);
+    document.querySelector('#weather-table').appendChild(tableBody);
+
+    // delete button
+    const deleteButtons = document.querySelectorAll('.delete-btn');
+    deleteButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        const row = button.closest('tr');
+        row.remove();
+      });
+    });
+  };
+}
 
 export default { render };
