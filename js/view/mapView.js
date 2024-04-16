f749015f487306d949d48e9695db7206fb53a234
jRocha <jose.f.rocha92@gmail.com>
2024-04-16 11:37:04 +0100

    add alert when city not found add the see more btn

--- js/view/mapView.js
+++ js/view/mapView.js
@@ -4,13 +4,22 @@ function render() {
   const container = document.querySelector('#container');
   container.innerHTML = '';
 
-  //search
+  // Search input and button
   const item = document.createElement('div');
-  item.className = `input-group mb-3 mt-3`;
-  item.innerHTML = `<input type="text" class="form-control box-sh" placeholder="Enter a city name" aria-label="Enter a city name" aria-describedby="button-addon2" id="city-name">
-                    <button class="btn btn-info" type="button" id="btn-weather">Search</button>`;
-
+  item.className = `input-group mb-3 mt-3 box-sh btn-weather`;
+  item.innerHTML = `<input type="text" class="form-control" placeholder="Enter a city name" aria-label="Enter a city name" aria-describedby="button-addon2" id="city-name">
+                    <button class="btn btn-primary" type="button" id="btn-weather">Search</button>`;
   container.appendChild(item);
+
+  // Alert message
+  const alertMessage = document.createElement('div');
+  alertMessage.className = 'd-none';
+  alertMessage.id = 'alert-message';
+  alertMessage.innerHTML = `<div class="alert alert-danger" role="alert">
+                              The selected city was not found. Please enter a valid city.
+                            </div>`;
+
+  container.appendChild(alertMessage);
 
   const btnWeather = document.getElementById('btn-weather');
 
@@ -22,6 +31,13 @@ function render() {
     const result = await weatherService.getWeatherByCity(cityInput.value);
     console.log(result);
 
+    // Check if city not found
+    if (result.cod === '404') {
+      showAlertMessage();
+      setTimeout(hideAlertMessage, 1800);
+      return;
+    }
+
     // table header creation
     if (!document.querySelector('#weather-table')) {
       const table = document.createElement('table');
@@ -31,14 +47,13 @@ function render() {
 
       const tableHeader = document.createElement('thead');
       tableHeader.innerHTML = `
-        <tr>
+        <tr class="tr-c">
           <th>City Name</th>
           <th></th>
           <th>Weather</th>
           <th>current Temp</th>
-          <th>Max Temp</th>
-          <th>Min Temp</th>
-          <th>Action</th>
+          <th>Check</th>
+          <th>Delete</th>
         </tr>
       `;
       table.appendChild(tableHeader);
@@ -52,8 +67,7 @@ function render() {
       <td><img src="https://openweathermap.org/img/wn/${result.weather[0].icon}@2x.png" alt="image icon" style="width: 35px"></td>
       <td>${result.weather[0].main}</td>
       <td>${result.main.temp} Cº</td>
-      <td>${result.main.temp_max} Cº</td>
-      <td>${result.main.temp_min} Cº</td>
+      <td><button class="btn btn btn-outline-success success-btn">See More</button></td>
       <td><button class="btn btn btn-outline-danger delete-btn">Delete</button></td>
     `;
     tableBody.appendChild(newRow);
@@ -68,6 +82,14 @@ function render() {
       });
     });
   };
+
+  function showAlertMessage() {
+    document.getElementById('alert-message').classList.remove('d-none');
+  }
+
+  function hideAlertMessage() {
+    document.getElementById('alert-message').classList.add('d-none');
+  }
 }
 
 export default { render };
